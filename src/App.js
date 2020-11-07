1712f88f155037a35b7c4dc5f17f7835676eee06
Your Name <darwinmuthu450@gmail.com>
2020-11-07 07:28:47 +0530

    init and data added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import "./App.css";
+// import CSK form './images/CSK.png';
+// import MI form './images/MI.png';
+import Header from "./Header.js";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      msg: "180",
+    };
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <div id="team1">CHENNAI SUPER KINGS</div>
+        {/* <img id="image1" src={CSk}/> */}
+        <div id="team2">MUMBAI INDIANS</div>
+        {/* <img id="image2" src={MI}/> */}
+        <h4>MI has to score {this.state.msg} against CSK to win this T20</h4>
+      </React.Fragment>
+    );
+  }
 }
-
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="match-header" id="match-header">
+        <div className="match-container">
+          <div className="match-header-section">
+            <div className="match-header-content align-center">Cricket T20</div>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+export default Header;
